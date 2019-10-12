94c62cb066b08e061d79f97e2a4880efbf70c747
Teemu Autto <teemu@rautakuu.org>
2019-10-12 19:51:17 +0300

    Variaatio etäisyyden laskemisesta. Ottaa huomioon vastausten poikkeavuuden.

--- demot/laske_etaisyys.py
+++ demot/laske_etaisyys.py
@@ -32,7 +32,7 @@ def etäisyys(tiedosto, määrä: int):
         # Muutetaan samalla tekstidata numeraaliseksi (`int`).
         # vertailtavat = vaalidata.head(2)
         vertailtavat = vaalidata.sample(2)
-        vastaukset = vertailtavat.iloc[:, 5:33].fillna(3).astype('int')
+        vastaukset = vertailtavat.iloc[:, 4:33].fillna(3).astype('int')
 
         # Laske ero omalla funktiolla.
         ero_mean = laske_ero_mean(vastaukset)
@@ -56,5 +56,62 @@ def laske_ero_mean(vastaukset: pd.DataFrame):
     return erot.mean(axis=1)
 
 
+@click.command()
+@click.argument("tiedosto", type=click.Path(exists=True), default=VAALIDATA_TIEDOSTO)
+def etäisyys_poikkeavuudella(tiedosto):
+    """
+    Laskee kahden satunnaisen ehdokkaan etäisyyden ottaen huomioon vastausten poikkeavuuden massasta.
+    """
+
+    # Asteikko jota kysymykset käyttävät.
+    ASTEIKON_SKAALA = 5
+
+    # Luetaan vaalidata
+    vaalidata = lue_data_sisään(tiedosto)
+
+    # Valitse satunnaiset ehdokkaat, ja skaalakysymykset.
+    vertailtavat = vaalidata.sample(2).fillna(3)
+    vastaukset = vaalidata.iloc[:, 4:33].fillna(3).astype('int')
+
+    print(vertailtavat.iloc[0]["puolue"], vertailtavat.iloc[0]["vaalipiiri"])
+    print(vertailtavat.iloc[1]["puolue"], vertailtavat.iloc[1]["vaalipiiri"])
+
+    keskiarvot = pd.Series()
+
+    for sarake in vastaukset:
+        # Käy vastaukset läpi yksitellen.
+
+        col = vastaukset[sarake]
+
+        # Kerää ehdokkaiden vastaukset. `set()` pitää huolta ettei sama vastaus toistu useampaan kertaan.
+        vertailtavien_vastaukset = set([
+            np.int(vertailtavat.iloc[0][sarake]),
+            np.int(vertailtavat.iloc[1][sarake])
+        ])
+
+        # Laske keskiarvo kuinka monta on vastannut samoin, ja kuinka monta erillailla.
+        # `isin()` luo listan True/False arvoilla, ja saadulla listalla suodatetaan vain True arvot. laskentaan.
+        # Tilde taas vaihtaa arvot toisinpäin.
+        samoin_vastanneita = col[col.isin(vertailtavien_vastaukset)].count() / len(vertailtavien_vastaukset)
+        muutoin_vastanneita = col[~col.isin(vertailtavien_vastaukset)].count() / (ASTEIKON_SKAALA - len(vertailtavien_vastaukset))
+
+        # Varmista arvo järkevälle skaalalle.
+        painotin = min(2.0, max(0.1, muutoin_vastanneita / samoin_vastanneita))
+
+        # Laske etäisyys kuten :func:`laske_ero_mean`. Pieni ero koska kyseessä on `Series` eikä `Dataframe`.
+        etäisyys = abs(vertailtavat[sarake].astype('int').diff().iat[1]) * painotin
+
+        # Tulostele debuggausta varten tietoa
+        print(sarake, vertailtavien_vastaukset)
+        print("\tSamoin:", samoin_vastanneita, "Muutoin:", muutoin_vastanneita, "Painotin", painotin)
+        print("\tEtäisyys", etäisyys)
+
+        keskiarvot = keskiarvot.append(pd.Series([etäisyys]), ignore_index=True)
+
+    etäisyys_mean = keskiarvot.mean()
+    print("Etäisyys mean", etäisyys_mean)
+    return etäisyys_mean
+
+
 if __name__ == "__main__":
-    etäisyys()
+    etäisyys_poikkeavuudella()
