0906017f653d5c36f4bb9bfbb3678f726de8844f
Joni Nisula <joni_ni@hotmail.com>
2019-11-25 16:00:06 +0200

    main päivitys ja main.js lisäys

---
+++ agora_analytica/flask/static/main.js
@@ -0,0 +1,14 @@
+// Käy läpi puolueiden ja vaalipiirien checkboxit ja päivitä graph datalla josta suodatettu ne mitä ei ole valittu
+function refreshGraph() {
+    //Käy läpi puolueet
+    array.forEach(element => {
+        
+    });
+    //Käy läpi vaalipiirit
+    array.forEach(element => {
+        
+    });
+    
+    //päivitä graph
+    //toteutus?
+}
