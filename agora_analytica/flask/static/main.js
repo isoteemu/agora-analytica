be897179bf1636ae9d30560eef466889d525d0b0
Joni Nisula <joni.p.nisula@student.jyu.fi>
2019-12-05 15:43:01 +0200

    modaalin toimivaksi

--- agora_analytica/flask/static/main.js
+++ agora_analytica/flask/static/main.js
@@ -2,6 +2,17 @@ function show_node_info(id){
 console.log("ddd", id)
 d3.json("/api/node/"+id+".json", function(err, data){
 console.log(data)
+
+/*console.log(data.name)
+console.log(data.vaalipiiri)
+console.log(data.party)*/
+
+$("#candidateName").text("Name: " + data.name)
+$("#candidateNumber").text("Candidate number: " + "TODO: candidate number here ")
+$("#candidateParty").text("Party: " + data.party)
+$("#candidateConstituency").text("Constituency: " + data.constituency)
+
+$("#candidateInformationModal").modal('show')
+
 })
-    $("#ehdokastiedotmodal").modal("show")
 }
